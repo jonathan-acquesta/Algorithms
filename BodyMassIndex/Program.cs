69331b58dceef65212cbc7c234a6a9c44b8d4307
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-04-21 13:01:11 -0300

    Final version BMI

--- BodyMassIndex/Program.cs
+++ BodyMassIndex/Program.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Threading;
 
 namespace BodyMassIndex
 {
     class Program
     {
+        #region Fields
+
+        private const string YesAnswear = "YES";
+
+        const string directory = @"C:\Projects\";
+
+        static string filePath = $@"{directory}bodyMassIndex.txt";
+
+        static CultureInfo culture = new CultureInfo("en-US");
+
+        #endregion
+
         static void Main(string[] args)
         {
-            //nome, idade, peso e altura
+            Thread.CurrentThread.CurrentCulture = culture;
 
+            CreateFile();
+            ShowDataInFile();
+
+            QuestionAboutAddNewItem();
+            var answear = Console.ReadLine();
+
+            while (answear.ToUpper() == YesAnswear)
+            {
+                AddNewItem();
+
+                Console.Clear();
+                ShowDataInFile();
+
+                QuestionAboutAddNewItem();
+                answear = Console.ReadLine();
+            }
+
+        }
+
+        private static void QuestionAboutAddNewItem()
+        {
+            Console.WriteLine("Do you want to inform a new person? (Yes or No)");
+        }
+
+        private static void AddNewItem()
+        {
             Console.Write("Name: ");
             string name = Console.ReadLine();
 
@@ -24,7 +64,7 @@ namespace BodyMassIndex
             {
                 Console.Write("Body Weight (Kg): ");
 
-            } while (!double.TryParse(Console.ReadLine(), out bodyWeight));
+            } while (!double.TryParse(Console.ReadLine().Replace(",", "."), out bodyWeight));
 
 
             double height = 0;
@@ -32,11 +72,11 @@ namespace BodyMassIndex
             {
                 Console.Write("Height (Meters): ");
 
-            } while (!double.TryParse(Console.ReadLine(), out height));
+            } while (!double.TryParse(Console.ReadLine().Replace(",", "."), out height));
 
 
-            double bodyMassIndex = Math.Pow((bodyWeight / height), 2);
-            
+            double bodyMassIndex = bodyWeight / (height * height);
+
             Console.WriteLine($"Body Mass Index: {bodyMassIndex}");
 
             ////Oldways to concat strings, just to show:
@@ -44,27 +84,47 @@ namespace BodyMassIndex
             //Console.WriteLine(string.Format("Body Mass Index: {0}", bodyMassIndex));
             //Console.WriteLine("Body Mass Index: " + bodyMassIndex);
 
-            string directory = @"C:\Projects\";
+            WriteDataInFile(name, age, bodyWeight, height, bodyMassIndex);
+        }
+
+        private static void WriteDataInFile(string name, int age, double bodyWeight, double height, double bodyMassIndex)
+        {
+            using (StreamWriter streamWriter = File.AppendText(filePath))
+            {
+                streamWriter.WriteLine(WriteFormatLine(name, age.ToString("N0", culture), bodyWeight.ToString("N2", culture), height.ToString("N2", culture), bodyMassIndex.ToString("N2", culture)));
+            }
+        }
+
+        private static void ShowDataInFile()
+        {
+            Console.WriteLine("Current data file".PadLeft(70, '-').PadRight(125, '-'));
+            Console.WriteLine("");
+
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine($"_".PadLeft(125, '_'));
+            Console.WriteLine("");
+        }
+
+        private static void CreateFile()
+        {
             if (!Directory.Exists(directory)) { Directory.CreateDirectory(directory); }
 
-            string filePath = $@"{directory}bodyMassIndex.txt";
-            if (!File.Exists(filePath)) 
+            if (!File.Exists(filePath))
             {
                 var file = File.CreateText(filePath);
                 file.WriteLine(WriteFormatLine("Name", "Age", "Body Weight", "Height", "Body Mass Index"));
                 file.Dispose();
             }
-
-            using (StreamWriter streamWriter =  File.AppendText(filePath))
-            {
-                streamWriter.WriteLine(WriteFormatLine(name, age.ToString(), bodyWeight.ToString(), height.ToString(), bodyMassIndex.ToString("N2")));
-            }
-
         }
 
         private static string WriteFormatLine(string name, string age, string bodyWeight, string height, string bodyMassIndex)
         {
-            return $"|{name.PadRight(2, ' '),-60}|{age.ToString().PadRight(2, ' '),-15}|{bodyWeight.PadRight(2, ' '),-15}|{height.PadRight(2, ' '),-15}|{bodyMassIndex.PadRight(2, ' '),-15}";
+            return $"|{name.PadRight(2, ' '),-60}|{age.PadRight(2, ' '),-15}|{bodyWeight.PadRight(2, ' '),-15}|{height.PadRight(2, ' '),-15}|{bodyMassIndex.PadRight(2, ' '),-15}";
         }
     }
 }
